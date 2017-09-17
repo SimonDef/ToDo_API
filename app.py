10ab56d13e5639f145b5231cec20d07159a09dc5
SimonDef <simon.defrenet@gmail.com>
2017-09-17 20:21:32 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,73 @@
+#!flask/bin/python
+
+from flask import Flask, jsonify, abort, make_response, request
+import json
+
+app = Flask(__name__)
+
+#load a json dictionnary
+with open('Tasks.json') as json_tasks:
+    tasks = json.load(json_tasks)
+    json_tasks.close()
+
+@app.route('/todo/api/v1.0/tasks', methods=['GET'])
+def get_tasks():
+    return jsonify({'tasks': tasks["Tasks"]})
+
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    task = [task for task in tasks["Tasks"] if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    return jsonify({'task': task[0]})
+
+@app.route('/todo/api/v1.0/tasks', methods=['POST'])
+def create_task():
+    if not request.json or not 'title' in request.json:
+        abort(400)
+    task = {
+        'id': tasks["Tasks"][-1]['id'] + 1,
+        'title': request.json['title'],
+        'who': request.json.get('who', ""),
+        'description': request.json.get('description', ""),
+        'done': False
+    }
+    tasks["Tasks"].append(task)
+    return jsonify({'task': task}), 201
+
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['DELETE'])
+def delete_task(task_id):
+    task = [task for task in tasks["Tasks"] if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+        tasks["Tasks"].remove(task[0])
+    return jsonify({'result': True})
+
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['PUT'])
+def update_task(task_id):
+    task = [task for task in tasks["Tasks"] if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    if not request.json:
+        abort(400)
+    if 'title' in request.json and type(request.json['title']) != unicode:
+        abort(400)
+    if 'who' in request.json and type(request.json['who']) is not unicode:
+        abort(400)
+    if 'description' in request.json and type(request.json['description']) is not unicode:
+        abort(400)
+    if 'done' in request.json and type(request.json['done']) is not bool:
+        abort(400)
+    task[0]['title'] = request.json.get('title', task[0]['title'])
+    task[0]['description'] = request.json.get('description', task[0]['description'])
+    task[0]['done'] = request.json.get('done', task[0]['done'])
+    return jsonify({'task': task[0]})
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, threaded=True)
